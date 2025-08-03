a11c421577b08d61a1e0b68e0bf147e95d1a0c7e
KinhLupNe <kinhlupna@gmail.com>
2025-08-03 22:47:24 +0700

    fix bug frame buffer

--- out/build/x64-debug/tools/PSLogger/cmake_install.cmake
+++ out/build/x64-debug/tools/PSLogger/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: E:/IT/devGame3D/Engine3D
+# Install script for directory: E:/IT/devGame3D/Engine3D/tools/PSLogger
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -37,31 +37,9 @@ if(NOT DEFINED CMAKE_OBJDUMP)
   set(CMAKE_OBJDUMP "C:/MinGW/bin/objdump.exe")
 endif()
 
-if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  # Include the install script for each subdirectory.
-  include("E:/IT/devGame3D/Engine3D/out/build/x64-debug/tools/PSLogger/cmake_install.cmake")
-
-endif()
-
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
 if(CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "E:/IT/devGame3D/Engine3D/out/build/x64-debug/install_local_manifest.txt"
-     "${CMAKE_INSTALL_MANIFEST_CONTENT}")
-endif()
-if(CMAKE_INSTALL_COMPONENT)
-  if(CMAKE_INSTALL_COMPONENT MATCHES "^[a-zA-Z0-9_.+-]+$")
-    set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
-  else()
-    string(MD5 CMAKE_INST_COMP_HASH "${CMAKE_INSTALL_COMPONENT}")
-    set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INST_COMP_HASH}.txt")
-    unset(CMAKE_INST_COMP_HASH)
-  endif()
-else()
-  set(CMAKE_INSTALL_MANIFEST "install_manifest.txt")
-endif()
-
-if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "E:/IT/devGame3D/Engine3D/out/build/x64-debug/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "E:/IT/devGame3D/Engine3D/out/build/x64-debug/tools/PSLogger/install_local_manifest.txt"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
